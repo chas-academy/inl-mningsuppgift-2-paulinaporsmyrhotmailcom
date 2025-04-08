09a5e79b53e7ab83b7214092dd0213b33a101a1a
paulina.porsmyr@hotmail.com <paulina.porsmyr@hotmail.com>
2025-04-08 20:21:58 +0200

    First commit

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+float fahrenheit_to_celsius(float f){
+    float c = (f-32) * 5.0 / 9.0;
+    return c;
+}
 
-// This is where you add the two functions
+float celsius_to_fahrenheit(float c){
+    float f = (c*9.0)/5.0 +32;
+    return f;
+}
 
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
 
-    printf("%.1f\n", fahrenheit_to_celsius(temp_f));  // Expected output: 37.8
-    printf("%.1f\n", celsius_to_fahrenheit(temp_c));  // Expected output: 98.6
+    printf("%.1f\n", fahrenheit_to_celsius(temp_f));
+    printf("%.1f\n", celsius_to_fahrenheit(temp_c));
 
     return 0;
 }
